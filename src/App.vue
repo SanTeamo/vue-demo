3873b3825a931a6cb45635e951d9736783026d72
SanTeamo <834713675@qq.com>
2021-11-12 17:10:29 +0800

    界面样式调整

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <!-- 我是头部 -->
-    <header>
-      <h2>我是头部</h2>
+    <header class="header">
+      <h2 style="text-align: center">Header</h2>
       <button @click="back">&lt;</button>
       <button @click="pre">&gt;</button>
     </header>
     <!-- 路由出口 -->
-    <transition name="fade" mode="out-in">
-      <router-view />
-    </transition>
+    <div class="content">
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+    </div>
     <!-- 路由以外的公共部分 -->
-    <footer>我是脚部</footer>
+    <footer class="footer">
+      <h2>Footer</h2>
+    </footer>
   </div>
 </template>
 
@@ -29,12 +33,27 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+#app {
+  position: relative;
+}
+.header {
+  position: relative;
+}
+.content {
+  position: relative;
+  height: 80%;
+  padding: 0 5px;
+}
+.footer {
+  position: relative;
+  text-align: center;
 }
 </style>
