22fc2079cae9148206efc2266c22e58e1ca2f3ff
SanTeAmo <834713675@qq.com>
2021-12-26 12:21:45 +0800

    element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <!-- 我是头部 -->
-    <header class="header">
-      <h2 style="text-align: center">Header</h2>
-      <button @click="back">&lt;</button>
-      <button @click="pre">&gt;</button>
-    </header>
-    <!-- 路由出口 -->
-    <div class="content">
-      <transition name="fade" mode="out-in">
-        <router-view />
-      </transition>
-    </div>
-    <!-- 路由以外的公共部分 -->
-    <footer class="footer">
-      <h2>Footer</h2>
-    </footer>
+    <el-container style="height: 100%">
+      <el-aside width="0">Aside</el-aside>
+      <el-container>
+        <el-header>
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-arrow-left" @click="back">后退</el-button>
+            <el-button type="primary" @click="pre">
+              前进
+              <i class="el-icon-arrow-right el-icon--right"></i>
+            </el-button>
+          </el-button-group>
+        </el-header>
+        <el-main>
+          <transition name="fade" mode="out-in">
+            <router-view />
+          </transition>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -33,27 +36,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-#app {
-  position: relative;
-}
-.header {
-  position: relative;
-}
-.content {
-  position: relative;
-  height: 80%;
-  padding: 0 5px;
-}
-.footer {
-  position: relative;
-  text-align: center;
-}
-</style>
+<style scoped></style>
